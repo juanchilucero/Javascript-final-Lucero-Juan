b3f2878421c46c393f26c5871c36ae7272940cd8
Juan Ignacio Lucero Vazquez <juanchi98lucero@gmail.com>
2023-12-07 09:47:09 -0300

    agregando puntajes

--- js/script.js
+++ js/script.js
@@ -8,6 +8,10 @@ let nivelActual = 0;
 let cartas = [];
 let cartasVolteadas = [];
 let cartasCoincidentes = [];
+let puntosBase = 1000;
+const tiempoLimite = 5 * 60 * 1000; // 5 minutos en milisegundos
+let tiempoInicio, tiempoFinal;
+let puntuacionTotal = 0
 
 function mezclar(array) {
   let currentIndex = array.length, temp, randomIndex;
@@ -25,6 +29,10 @@ function mezclar(array) {
 }
 
 function crearTablero() {
+
+  cartasCoincidentes = [];
+  tiempoInicio = Date.now();
+
   const nivel = niveles[nivelActual];
   const imagenesParaNivel = nivel.imagenes.slice(0, nivel.parejas);
   const imagenesDuplicadas = imagenesParaNivel.concat(imagenesParaNivel);
@@ -59,6 +67,37 @@ function voltearCarta() {
   }
 }
 
+
+// Función para guardar el puntaje del usuario en el localStorage
+function guardarPuntaje(nombreUsuario, puntaje) {
+  let puntajes = JSON.parse(localStorage.getItem('puntajes')) || [];
+  puntajes.push({ nombre: nombreUsuario, puntaje });
+  puntajes.sort((a, b) => b.puntaje - a.puntaje); // Ordena los puntajes de mayor a menor
+  puntajes = puntajes.slice(0, 10); // Solo guarda los 10 mejores puntajes
+
+  localStorage.setItem('puntajes', JSON.stringify(puntajes));
+}
+
+// Función para mostrar los mejores puntajes en el div con la clase "mejoresPuntuaciones"
+function mostrarMejoresPuntuaciones() {
+  const puntajes = JSON.parse(localStorage.getItem('puntajes')) || [];
+  const mejoresPuntuacionesDiv = document.querySelector('.mejoresPuntuaciones');
+
+  // Limpia el contenido anterior antes de mostrar los puntajes
+  mejoresPuntuacionesDiv.innerHTML = '';
+
+  puntajes.forEach((puntaje, index) => {
+    const parrafo = document.createElement('p');
+    parrafo.textContent = `${index + 1}. ${puntaje.nombre}: ${puntaje.puntaje}`;
+    mejoresPuntuacionesDiv.appendChild(parrafo);
+  });
+}
+
+
+
+
+// verificacion coincidencia
+
 function verificarCoincidencia() {
   const [carta1, carta2] = cartasVolteadas;
   const imagen1 = carta1.querySelector('.contenido-carta').src;
@@ -75,18 +114,45 @@ function verificarCoincidencia() {
 
   cartasVolteadas = [];
 
-  if (cartasCoincidentes.length === cartas.length) {
+  // Verifica si todas las cartas coincidentes se han encontrado
+  if (cartasCoincidentes.length === niveles[nivelActual].parejas * 2) {
+    // Si todas las cartas coincidentes han sido encontradas, pasa al siguiente nivel
     setTimeout(() => {
       if (nivelActual < niveles.length - 1) {
         nivelActual++;
+        tiempoFinal = Date.now();
         alert('¡Pasaste al siguiente nivel!');
+
+
+        const tiempoTranscurrido = tiempoFinal - tiempoInicio;
+        let tiempoPenalizacion = Math.floor(tiempoTranscurrido / 1000) * 2;
+        const puntuacionNivel = puntosBase - tiempoPenalizacion;
+
+        // Sumar la puntuación del nivel actual a la puntuación total
+        puntuacionTotal += puntuacionNivel;
+        console.log(`Tu puntuación actual es: ${puntuacionNivel}`);
+        console.log(`Tu puntuación total hasta el momento es: ${puntuacionTotal}`);
+
+        // Restablece las variables de seguimiento de las cartas
+        cartas = [];
+        cartasVolteadas = [];
+        cartasCoincidentes = [];
+
         crearTablero();
       } else {
         alert('¡Has completado todos los niveles! ¡Has ganado!');
+        console.log(`Tu puntuación final es: ${puntuacionTotal}`);
+
+        // Guardar la puntuación total en el localStorage
+        guardarPuntaje(nombreUsuario, puntuacionTotal);
+        // Mostrar las mejores puntuaciones después de guardar la puntuación
+        mostrarMejoresPuntuaciones();
       }
     }, 500);
   }
 }
+
+
 function iniciarJuego() {
   const nombreUsuario = document.getElementById('username').value;
   if (nombreUsuario.trim() !== '') {
